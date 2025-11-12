bd7897677d71bc68a73a45d6ef1f5d6352bfdf37
LongNT <longtn@tora-tech.com>
2025-11-12 16:37:01 +0700

    add export-to-excel API route

--- app/api/export-to-excel/route.ts
+++ app/api/export-to-excel/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 import { insertPowerAppsRecords, parsePowerAppsPayload } from '@/lib/powerApps';
 
 export async function POST(request: Request) {
+  console.log('aaaaaaaaaaaaaaaa', request);
   try {
     const raw = await request.text();
     console.log('Raw body from Flow:', raw);
@@ -34,6 +35,6 @@ export async function POST(request: Request) {
   }
 }
 
-export async function GET() {
-  return NextResponse.json({ message: 'API is live' });
-}
+// export async function GET() {
+//   return NextResponse.json({ message: 'API is live' });
+// }
